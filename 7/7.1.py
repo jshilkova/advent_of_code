fd65979f59fc6052b7dfee8375dd2900fb774948
Julia Shilkova <yulia.shilkova@gmail.com>
2023-12-12 19:50:04 +0400

    day 7 optimize dict initiation

--- 7/7.1.py
+++ 7/7.1.py
@@ -17,8 +17,7 @@ def get_hand_and_bids():
 
 
 def get_combo_coefficient(hand):
-    number_of_a_kind = \
-        {"2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "T": 0, "J": 0, "Q": 0, "K": 0, "A": 0}
+    number_of_a_kind = dict((k, 0) for k in "J23456789TJQKA")
     for card in hand:
         number_of_a_kind[card] += 1
     if 5 in number_of_a_kind.values():
